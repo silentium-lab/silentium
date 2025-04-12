b666fefc57b8ebd1dfdc70ab155cb0e8ff98ee6f
Eugene Kosukhin <kosuha606@gmail.com>
2025-04-12 20:21:12 +0300

    feat(main): favicon fixed

--- docs/assets/js/index.mjs
+++ docs/assets/js/index.mjs
@@ -57,7 +57,7 @@ routesTransport.result().value(
           url: "/",
           template: "pages/index.html",
           aliases: [basePath, `${basePath}index.html`, ""],
-          page: new Page("Patron Components"),
+          page: new Page("Silentium"),
         },
         ...dynamicRoutes,
         {
