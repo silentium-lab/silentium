d08f732339d75e4774709f0cd6717f43231c6c99
Eugene Kosukhin <kosuha606@gmail.com>
2025-04-23 16:48:40 +0300

    feat(16-issue): remove unneeded patron class

--- src/Patron/Patron.ts
+++ src/Patron/Patron.ts
@@ -1,11 +1,15 @@
 import { give, GuestType } from "../Guest/Guest";
 import { GuestDisposableType } from "../Guest/GuestDisposable";
 
+export type PatronType<T> = GuestDisposableType<T> & {
+  introduction(): "patron";
+};
+
 /**
  * Helps to check what incoming object is patron
  * @url https://silentium-lab.github.io/silentium/#/utils/is-patron
  */
-export const isPatron = (guest: GuestType): guest is Patron<unknown> =>
+export const isPatron = (guest: GuestType): guest is PatronType<unknown> =>
   typeof guest === "object" &&
   guest !== null &&
   guest?.introduction?.() === "patron";
@@ -37,25 +41,3 @@ export const patron = <T>(
 
   return result;
 };
-
-export class Patron<T> implements GuestDisposableType<T> {
-  public constructor(private willBePatron: GuestType<T>) {
-    if (willBePatron === undefined) {
-      throw new Error("Patron didnt receive willBePatron argument");
-    }
-  }
-
-  public introduction() {
-    return "patron" as const;
-  }
-
-  public give(value: T): this {
-    give(value, this.willBePatron);
-    return this;
-  }
-
-  public disposed(value: T | null): boolean {
-    const maybeDisposable = this.willBePatron as GuestDisposableType;
-    return maybeDisposable?.disposed?.(value) || false;
-  }
-}
