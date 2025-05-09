eaeb91a0960517c41959cb51b7f0c43fb74d054c
Eugene Kosukhin <kosuha606@gmail.com>
2025-05-09 16:45:47 +0300

    feat(37-issue): sourceResettable implemented

--- src/Source/SourceResettable.test.ts
+++ src/Source/SourceResettable.test.ts
@@ -1,5 +1,24 @@
-import { expect, test } from "vitest";
+import { sourceOf } from "../Source/SourceChangeable";
+import { sourceResettable } from "../Source/SourceResettable";
+import { sourceSync } from "../Source/SourceSync";
+import { expect, test, vi } from "vitest";
 
-test("SourceResettable.test", () => {
-  expect(123).toBe(123);
+test("SourceFiltered.test", () => {
+  const resetSrc = sourceOf();
+  const baseSrc = sourceOf<number>(1);
+  const resettable = sourceResettable(baseSrc, resetSrc);
+  const resettableSync = sourceSync(resettable);
+
+  expect(resettableSync.syncValue()).toBe(1);
+
+  resetSrc.give(1);
+
+  const g1 = vi.fn();
+  resettable.value(g1);
+  expect(g1).not.toBeCalled();
+
+  baseSrc.give(2);
+  const g2 = vi.fn();
+  resettable.value(g2);
+  expect(g2).toBeCalledWith(2);
 });
