16829680d7d4547456b8a1c60b0f676643645958
Eugene Kosukhin <kosuha606@gmail.com>
2025-05-09 12:06:22 +0300

    feat(37-issue): work on source resettable

--- src/Source/SourceOnce.test.ts
+++ src/Source/SourceOnce.test.ts
@@ -2,7 +2,7 @@ import { expect, test, vitest } from "vitest";
 import { sourceOnce } from "./SourceOnce";
 
 test("SourceOnce.test", () => {
-  const source = sourceOnce(123);
+  const source = sourceOnce<number>(123);
   source.give(321);
   const g = vitest.fn();
   source.value(g);
---
+++ src/Source/SourceResettable.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest";
+
+test("SourceResettable.test", () => {
+  expect(123).toBe(123);
+});
