54d506af8e44d111a0ae666ad332689342d96658
Eugene Kosukhin <kosuha606@gmail.com>
2025-05-25 11:16:50 +0300

    docs(main): documentation fixed

--- docs/build.sh
+++ docs/build.sh
@@ -3,11 +3,11 @@
 echo "BEGIN"
 cd docs
 cp template.html index.html
-sed -i "s/TPL_SILENTIUM_OOP/https:\/\/cdn.jsdelivr.net\/npm\/silentium@0.0.27\/dist\/silentium.min.mjs/g" "index.html"
+sed -i "s/TPL_SILENTIUM_OOP/https:\/\/cdn.jsdelivr.net\/npm\/silentium@0.0.34\/dist\/silentium.min.mjs/g" "index.html"
 
-sed -i "s/TPL_SILENTIUM_WEB_API/https:\/\/cdn.jsdelivr.net\/npm\/silentium-web-api@0.0.6\/dist\/silentium-web-api.min.mjs/g" "index.html"
+sed -i "s/TPL_SILENTIUM_WEB_API/https:\/\/cdn.jsdelivr.net\/npm\/silentium-web-api@0.0.7\/dist\/silentium-web-api.min.mjs/g" "index.html"
 
-sed -i "s/TPL_SILENTIUM_COMPONENTS/https:\/\/cdn.jsdelivr.net\/npm\/silentium-components@0.0.6\/dist\/silentium-components.min.mjs/g" "index.html"
+sed -i "s/TPL_SILENTIUM_COMPONENTS/https:\/\/cdn.jsdelivr.net\/npm\/silentium-components@0.0.20\/dist\/silentium-components.min.mjs/g" "index.html"
 
 
 cp template.html index-dev.html
