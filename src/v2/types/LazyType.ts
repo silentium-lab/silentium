342ffb58a76ebc4fec532a02d064813d6aa0a9c1
Eugene Kosukhin <kosuha606@gmail.com>
2025-07-26 23:30:13 +0300

    feat(pure): map component

--- src/v2/types/LazyType.ts
+++ src/v2/types/LazyType.ts
@@ -3,4 +3,4 @@ import { InformationType } from "./InformationType";
 /**
  * Lazy accepts any number of arguments and returns information
  */
-export type LazyType<T> = (...args: unknown[]) => InformationType<T>;
+export type LazyType<T> = (...args: any[]) => InformationType<T>;
