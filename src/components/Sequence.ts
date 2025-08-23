79fc26149fc0f8681eea958c2ea720bca0afad93
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-23 14:54:14 +0300

    fix(back_to_oop): sequence component

--- src/components/Sequence.ts
+++ src/components/Sequence.ts
@@ -1,17 +1,23 @@
-import { InformationType } from "../types";
+import { From, TheInformation, TheOwner } from "../base";
 
 /**
  * A component that takes one value at a time and returns
  * an array of all previous values
  * https://silentium-lab.github.io/silentium/#/en/information/sequence
  */
-export const sequence = <T>(base: InformationType<T>): InformationType<T[]> => {
-  return (o) => {
-    const result: T[] = [];
+export class Sequence<T> extends TheInformation<T[]> {
+  public constructor(private baseSrc: TheInformation<T>) {
+    super([baseSrc]);
+  }
 
-    base((v) => {
-      result.push(v);
-      o(result);
-    });
-  };
-};
+  public value(o: TheOwner<T[]>): this {
+    const result: T[] = [];
+    this.baseSrc.value(
+      new From((v) => {
+        result.push(v);
+        o.give(result);
+      }),
+    );
+    return this;
+  }
+}
