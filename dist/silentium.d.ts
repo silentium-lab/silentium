760572fbcf2213e696c9dc879dabf11d9f1088f4
Eugene Kosukhin <kosuha606@gmail.com>
2025-10-29 23:02:55 +0300

    build: before release

--- dist/silentium.d.ts
+++ dist/silentium.d.ts
@@ -130,7 +130,7 @@ declare function Component<T, P extends Array<any>>(executor: (this: TransportTy
 type ConstructableType = {
     new (...args: any[]): any;
 };
-declare function ComponentClass<T extends ConstructableType>(classConstructor: T): <R = null>(...args: ConstructorParameters<T>) => R extends null ? ConstructorParameters<T>[0] extends EventType ? InstanceType<T> extends SourceType ? SourceType<EventTypeValue<ConstructorParameters<T>[0]>> : EventType<EventTypeValue<ConstructorParameters<T>[0]>> : InstanceType<T> : R extends EventType ? R : EventType<R>;
+declare function ComponentClass<T extends ConstructableType>(classConstructor: T): <R = null>(...args: ConstructorParameters<T>) => R extends null ? ConstructorParameters<T>[0] extends EventType ? InstanceType<T> extends SourceType ? InstanceType<T> extends DestroyableType ? SourceType<EventTypeValue<ConstructorParameters<T>[0]>> & DestroyableType : SourceType<EventTypeValue<ConstructorParameters<T>[0]>> : InstanceType<T> extends DestroyableType ? EventType<EventTypeValue<ConstructorParameters<T>[0]>> & DestroyableType : EventType<EventTypeValue<ConstructorParameters<T>[0]>> : InstanceType<T> : R extends EventType ? R : EventType<R>;
 
 /**
  * An object that allows collecting all disposable objects and
